9ee321625e3c26a42afc5f439fab1784973e930e
Mai Anh Vu <maianhuel@gmail.com>
2024-11-15 16:51:15 -0600

    update

--- nystromformer/test_simple.py
+++ nystromformer/test_simple.py
@@ -1,13 +1,15 @@
 import tensorflow as tf 
 import sys
 import os
+import sys 
 
+PRJ_DIR = os.environ.get("PROJECT_DIR")
+sys.path.append(os.path.join(PRJ_DIR))
 
-
-from .nystrom_attention import  * 
-from .nystromformer import * 
+from nystromformer.nystrom_attention import  * 
+from nystromformer.nystromformer import * 
 
 
 
 if __name__ == "__main__":
-    print("dome import ")
+    print(">> dome import ")
