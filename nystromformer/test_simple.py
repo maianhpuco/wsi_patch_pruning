8cb98992f0e8331af6d3b3b0871c3f4387fb487a
Mai Anh Vu <maianhuel@gmail.com>
2024-11-15 17:28:50 -0600

    update

--- nystromformer/test_simple.py
+++ nystromformer/test_simple.py
@@ -5,7 +5,7 @@ import sys
 
 PRJ_DIR = os.environ.get("PROJECT_DIR")
 sys.path.append(os.path.join(PRJ_DIR))
-#sys.path.append(os.path.join(PRJ_DIR, "nystromformer"))
+sys.path.append(os.path.join(PRJ_DIR, "nystromformer"))
 
 from nystromformer.nystrom_attention import NystromAttention  
 from nystromformer.nystrom_former import Nystromformer
