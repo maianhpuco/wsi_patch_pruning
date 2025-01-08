ffe7aace2e18d59b5c7bca428285778c038686f7
Mai Anh Vu <maianhuel@gmail.com>
2025-01-07 20:27:43 -0600

    update

--- data/merge_dataset.py
+++ data/merge_dataset.py
@@ -8,6 +8,7 @@ import pandas as pd
 from sklearn.preprocessing import normalize
 import glob
 import openslide 
+import json 
 
 example_list = ['normal_072', 'normal_001', 'normal_048', 'tumor_026', 'tumor_031', 'tumor_032']
 SLIDE_PATH = '/project/hnguyen2/hqvo3/Datasets/digital_pathology/public/CAMELYON16/images'
