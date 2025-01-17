0c7d33ad675291e7994a78d466e532aa80f68b87
Mai Anh Vu <maianhuel@gmail.com>
2025-01-16 21:11:27 -0600

    update

--- data/merge_dataset.py
+++ data/merge_dataset.py
@@ -135,7 +135,7 @@ class SuperpixelDataset(Dataset):
         superpixel_downsampling = self.superpixel_labels == foreground_idx
         superpixel_extrapolated = self.extrapolate_superpixel_mask_segment(
             superpixel_downsampling, foreground_idx, bounding_boxes, downsample_factor)
-
+        print("down sampling check", superpixel_downsampling.shape)
         return (foreground_idx, xywh_abs_bbox, superpixel_extrapolated)
 
     @staticmethod
