5c7fa0cd59d29339bda711204822bfb5da82a1b7
Mai Anh Vu <maianhuel@gmail.com>
2025-01-20 15:28:22 -0600

    update

--- utils/utils.py
+++ utils/utils.py
@@ -32,6 +32,10 @@ def read_region_from_npy(dir_folder, slide_basename, foreground_idx):
 
 # Function to set up the logger
 def setup_logger(log_file="training_log.txt"):
+    log_dir = os.path.dirname(log_file)
+    if log_dir and not os.path.exists(log_dir):
+        os.makedirs(log_dir)  # Create directory if it doesn't exist 
+        
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)  # Set the log level to INFO to capture all log messages
     
