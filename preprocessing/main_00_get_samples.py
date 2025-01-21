202c9c85efb8730d8803be2988e6f3d4351e4be6
Mai Anh Vu <maianhuel@gmail.com>
2025-01-21 14:06:24 -0600

    update

--- preprocessing/main_00_get_samples.py
+++ preprocessing/main_00_get_samples.py
@@ -11,19 +11,29 @@ def load_config(config_file):
     return config
  
  
- 
 def main(args):
-    # List all items in slide_path
-    slide_items = os.listdir(args.slide_path)
+    # List all items in slide_path with .tif files
+    slide_items = [i.split(".")[0] for i in os.listdir(args.slide_path) if i.endswith('tif')]
     print("Items in slide path:")
     for item in slide_items:
         print(item)
 
-    # List all items in json_path
-    json_items = os.listdir(args.json_path)
+    # List all items in json_path with .json files
+    json_items = [i.split('.')[0] for i in os.listdir(args.json_path) if i.endswith('json')]
     print("Items in json path:")
     for item in json_items:
         print(item)
+
+    # Find items in slide_items that are not in json_items
+    items_not_in_json = [item for item in slide_items if item not in json_items]
+    
+    # Randomly sample 20 items from those not in json
+    sampled_items = random.sample(items_not_in_json, min(20, len(items_not_in_json)))
+
+    print("Randomly sampled items not in json path:")
+    for item in sampled_items:
+        print(item) 
+        
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -32,6 +42,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
     if os.path.exists(f'./testbest_config/{args.config_file}.yaml'):
         config = load_config(f'./testbest_config/{args.config_file}.yaml')
+        args.use_features = config.get('use_features', True)
           
     args.slide_path = config.get('SLIDE_PATH')
     args.json_path = config.get('JSON_PATH')    
