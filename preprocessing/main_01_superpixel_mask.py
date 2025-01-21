c7ab6c2e22694e139804ffec0ee843453a7364d4
Mai Anh Vu <maianhuel@gmail.com>
2025-01-21 14:26:48 -0600

    update

--- preprocessing/main_01_superpixel_mask.py
+++ preprocessing/main_01_superpixel_mask.py
@@ -201,8 +201,9 @@ def main(args):
     for wsi_basename in wsi_basenames:
         print(wsi_basename)
         slide_path = glob.glob(os.path.join(args.slide_path, f'{wsi_basename}*'))
-        print(slide_path)
-        # processing_superpixel(slide_path, JSON_SAVE_PATH) 
+        JSON_SAVE_PATH = os.path.join(args.json_path, wsi_basename)
+        print(JSON_SAVE_PATH) 
+        processing_superpixel(slide_path, JSON_SAVE_PATH) 
 
 
 
