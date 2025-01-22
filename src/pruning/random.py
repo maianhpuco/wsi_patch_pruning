4ebfe0573bff6553c16c680ab839350c5fefbafe
Mai Anh Vu <maianhuel@gmail.com>
2025-01-22 00:04:15 -0600

    update

--- src/pruning/random.py
+++ src/pruning/random.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import torch 
 # def random_feature_selection(features, patch_indices, pruning_rate=0.1):
 #     total_sample = features.shape[0]
 #     n_to_select = int(total_sample * (1-pruning_rate))
