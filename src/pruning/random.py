f1413577cf7f478c6e918e87b4da11c16e300d64
Mai Anh Vu <maianhuel@gmail.com>
2025-01-21 23:44:19 -0600

    update

--- src/pruning/random.py
+++ src/pruning/random.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def random_feature_selection(features, , pruning_rate=0.1):
+def random_feature_selection(features, patch_indices, pruning_rate=0.1):
     total_sample = features.shape[0]
     n_to_select = int(total_sample * (1-pruning_rate))
     selected_indices = np.random.choice(
