50e41ec0813ca7e45386dda52f10d14c9eb8fd88
Mai Anh Vu <maianhuel@gmail.com>
2025-01-21 23:44:43 -0600

    update

--- src/pruning/random.py
+++ src/pruning/random.py
@@ -5,4 +5,4 @@ def random_feature_selection(features, patch_indices, pruning_rate=0.1):
     n_to_select = int(total_sample * (1-pruning_rate))
     selected_indices = np.random.choice(
         range(total_sample), size=n_to_select, replace=False)
-    return features[selected_indices, :], patch_indices[selected_feature_indices,: ] 
+    return features[selected_indices, :], patch_indices[selected_indices,: ] 
