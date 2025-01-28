b866408aa97f6eca0971472da42f58931faa9aba
zach <bui.lam.quang.ngoc@moneyforward.vn>
2025-01-28 13:02:30 +0700

    comment transform

--- main_testbed_dino.py
+++ main_testbed_dino.py
@@ -81,7 +81,7 @@ def main(args):
     transform = transforms.Compose([
         transforms.Resize((224, 224)),  # Resize the image to 224x224
         transforms.ToTensor(),          # Convert the image to tensor
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),  # Normalize with ImageNet stats
+        #transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),  # Normalize with ImageNet stats
     ]) 
     
     print("len(wsi_paths)", len(wsi_paths))
