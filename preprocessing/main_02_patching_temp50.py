a10b6466a44a3c67ee07a0f2d43a6eb39e829b3c
Mai Anh Vu <maianhuel@gmail.com>
2025-01-29 11:41:39 -0600

    update

--- preprocessing/main_02_patching_temp50.py
+++ preprocessing/main_02_patching_temp50.py
@@ -286,9 +286,9 @@ if __name__ == '__main__':
         # the_rest = items_not_in_json[:-50]
         the_rest = [i for i in items_not_in_json if i not in last_50]
         
-        final_example_list = last_50 
+        final_example_list = the_rest 
         
-        print(last_50)
+        print(the_rest)
      
         print("Total number to process:", len(final_example_list))
         
