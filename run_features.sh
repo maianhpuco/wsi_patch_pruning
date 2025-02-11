9f80b9604fce1468a8fc88cfd58df580a4af1c4c
Mai Anh Vu <maianhuel@gmail.com>
2025-02-11 12:12:12 -0600

    update

--- run_features.sh
+++ run_features.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while true; do
-    python preprocessing/main_03_get_feature_draft.py
+    python preprocessing/main_03_get_features_draft.py
     echo "Process was killed. Restarting in 5 seconds..."
     sleep 5
 done
