cb7695f4fc7fb3d706bb1a47d3b572bb245130dc
Mai Anh Vu <maianhuel@gmail.com>
2025-02-11 23:47:39 -0600

    update

--- preprocessing/main_03_get_features_draft.py
+++ preprocessing/main_03_get_features_draft.py
@@ -136,7 +136,8 @@ if __name__ == '__main__':
     # items_to_process = [item for item in items_to_process if item not in remove_item] 
   
     example_list = np.random.permutation(items_to_process)
-     
+    
+    print("----> Total example list:" len(example_list)) 
     
     # args.example_list = ['tumor_026']  # Modify for testing
     # args.example_list = ['normal_114', 'tumor_026', 'tumor_009', 'tumor_024', 'tumor_015', 'normal_076','normal_070', 'normal_066', 'normal_053', 'normal_104','normal_112']   
