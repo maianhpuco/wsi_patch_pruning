94e851f8d2e342fb269cbf01cf0e882f57abc9d3
zach <bui.lam.quang.ngoc@moneyforward.vn>
2025-02-19 07:04:09 +0700

    run preprocessing normal_091, normal_144

--- preprocessing/main_01_superpixel_mask.py
+++ preprocessing/main_01_superpixel_mask.py
@@ -31,9 +31,7 @@ from utils import utils
 
 from torchvision import transforms 
 
-wsi_basenames = ['normal_031', 'tumor_024', 'normal_047', 'tumor_009', 'tumor_057', 'normal_093', 'normal_051', 'tumor_014', 'tumor_015', 'tumor_067', 'normal_003', 'tumor_084', 'tumor_101', 'normal_148', 'normal_022', 'tumor_012', 'normal_039', 'normal_084', 'normal_101', 'tumor_010', 'normal_088', 'normal_155', 'normal_087', 'normal_016', 'normal_114', 'normal_024', 'tumor_048', 'normal_078', 'tumor_049', 'tumor_086']
-
-
+wsi_basenames = ['normal_091', 'normal_144']
 
 def load_config(config_file):
     # Load configuration from the provided YAML file
